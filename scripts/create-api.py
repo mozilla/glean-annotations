adeb85d729312d47f59d70b386007ff5a244ec6e
William Lachance <wlachance@mozilla.com>
2021-04-29 15:06:22 -0400

    Support app annotations and add one for Firefox Desktop (#25)
    * Indicate that the main reference for this app should still be
      the probe dictionary for now
    * Provide a reference to Firefox on Glean
    
    Co-authored-by: Chris H-C <chutten@users.noreply.github.com>
    Co-authored-by: Jeff Klukas <jklukas@mozilla.com>

--- scripts/create-api.py
+++ scripts/create-api.py
@@ -11,26 +11,38 @@ ANNOTATIONS_DIR = os.path.join(os.path.dirname(__file__), "..", "annotations")
 data = defaultdict(lambda: defaultdict(lambda: {}))
 
 apps = os.listdir(ANNOTATIONS_DIR)
+
+
+def read_annotation(filename):
+    annotation_md = frontmatter.load(filename)
+
+    annotation = {"content": annotation_md.content}
+    for key in ["component", "features", "warning"]:
+        if annotation_md.get(key):
+            annotation[key] = annotation_md[key]
+
+    return annotation
+
+
 for app in apps:
+    try:
+        data[app]["app"] = read_annotation(
+            os.path.join(ANNOTATIONS_DIR, app, "README.md")
+        )
+    except FileNotFoundError:
+        # no top-level annotation for this application
+        pass
+
     for annotation_type in ("metrics", "pings"):
         annotation_dir = os.path.join(ANNOTATIONS_DIR, app, annotation_type)
         if not os.path.isdir(annotation_dir):
             # for some apps, we may have annotations for one annotation type
             # but not another
             continue
-        annotation_ids = os.listdir(
-            annotation_dir
-        )
+        annotation_ids = os.listdir(annotation_dir)
         for annotation_id in annotation_ids:
-            annotation_md = frontmatter.load(
-                os.path.join(
-                    annotation_dir, annotation_id, "README.md"
-                )
+            data[app][annotation_type][annotation_id] = read_annotation(
+                os.path.join(annotation_dir, annotation_id, "README.md")
             )
-            annotation = {"content": annotation_md.content}
-            for key in ["component", "features"]:
-                if annotation_md.get(key):
-                    annotation.update({key: annotation_md[key]})
-            data[app][annotation_type][annotation_id] = annotation
 
 print(json.dumps(data))
